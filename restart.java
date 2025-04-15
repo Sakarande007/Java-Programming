6c4c06cf7a2b3219cb4ed912e6570e8b34cf90ee
Sakarande007 <sakarande007@gmail.com>
2025-04-15 17:24:35 +0530

    Half Pyramid Code

--- restart.java
+++ restart.java
@@ -11,7 +11,7 @@ class restart {
         // int n = 5;
         // int m = 4;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
